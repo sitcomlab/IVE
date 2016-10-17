e495fb2874ba94004c561233ce1520781795299c
Nicho <nicho90@web.de>
2016-10-17 19:25:34 +0200

    Added favicons

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var neo4j = require('neo4j-driver').v1;
-// var pg = require('pg');
 
 
+// Create server
 var app = express();
 var httpPort = process.env.PORT || 5000;
 var httpsPort = httpPort + 443;
 var username = process.env.USERNAME || 'neo4j';
 var password = process.env.PW || 'neo4j';
+
+// Connect to Neo4j
 var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic(username, password));
 exports.driver = driver;
 var session = driver.session();
@@ -22,7 +24,6 @@ session
         console.log(colors.green(new Date() + " Neo4j is running on Port 7474"));
     })
     .catch(function(err) {
-        //console.error(err);
         console.log(colors.red(new Date() + " Neo4j could not been accessed!"));
     });
 
@@ -39,26 +40,24 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname + '/public'));
 
 
-// Load dependencies
+// Load routes for API
 var scenarios = require('./routes/scenarios');
 var locations = require('./routes/locations');
 var videos = require('./routes/videos');
 var overlays = require('./routes/overlays');
-
-// Load Routes
 app.use('/api', scenarios);
 app.use('/api', locations);
 app.use('/api', videos);
 app.use('/api', overlays);
 
 
-// Start Webserver
+// Start webserver
 var httpServer = http.createServer(app);
 httpServer.listen(httpPort, function() {
     console.log(colors.green(new Date() + " HTTP-Server is listening at port " + httpPort));
 });
 
-
+// Start websocket server
 var io = require('socket.io')(httpServer);
 exports.io = io;
 var sockets = require('./controllers/sockets.js').sockets;
