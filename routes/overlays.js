b403a24ab5fb9e11f72db364fe58dc456eb60e39
Nicho <nicho90@web.de>
2016-12-18 00:53:20 +0100

    Updated routes #8

--- routes/overlays.js
+++ routes/overlays.js
@@ -2,34 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 var list = require('../controllers/overlays/list');
+var post = require('../controllers/overlays/post');
+var del_all = require('../controllers/overlays/delete_all');
+
+var get = require('../controllers/overlays/get');
+var put = require('../controllers/overlays/put');
+var del = require('../controllers/overlays/delete');
+
 var list_by_scenario = require('../controllers/overlays/list_by_scenario');
 var list_by_video = require('../controllers/overlays/list_by_video');
-//var post = require('../controllers/overlays/post');
-var get = require('../controllers/overlays/get');
-//var put = require('../controllers/overlays/put');
-//var del = require('../controllers/overlays/delete');
+
 
 
 // LIST
 router.get('/overlays', list.request);
 
-// LIST BY SCENARIO
-router.get('/scenarios/:scenario_id/overlays', list_by_scenario.request);
-
-// LIST BY VIDEO
-router.get('/videos/:video_id/overlays', list_by_video.request);
-
 // POST
-//router.post('/overlays', post.request);
+router.post('/overlays', post.request);
+
+// DELETE ALL
+route.delete('/overlays', del_all.request);
+
 
 // GET
 router.get('/overlays/:overlay_id', get.request);
 
 // PUT
-//router.put('/overlays/:overlay_id', put.request);
+router.put('/overlays/:overlay_id', put.request);
 
 // DELETE
-//router.delete('/overlays/:overlay_id', del.request);
+router.delete('/overlays/:overlay_id', del.request);
+
+
+// LIST BY SCENARIO
+router.get('/scenarios/:scenario_id/overlays', list_by_scenario.request);
+
+
+// LIST BY VIDEO
+router.get('/videos/:video_id/overlays', list_by_video.request);
 
 
 module.exports = router;
--- routes/scenarios.js
+++ routes/scenarios.js
@@ -4,9 +4,11 @@ var router = express.Router();
 var list = require('../controllers/scenarios/list');
 var post = require('../controllers/scenarios/post');
 var del_all = require('../controllers/scenarios/delete_all');
+
 var get = require('../controllers/scenarios/get');
 var put = require('../controllers/scenarios/put');
 var del = require('../controllers/scenarios/delete');
+
 
 
 // LIST
@@ -17,6 +19,7 @@ router.post('/scenarios', post.request);
 
 // DELETE ALL
 router.delete('/scenarios', del_all.request);
+
 
 // GET
 router.get('/scenarios/:scenario_id', get.request);
