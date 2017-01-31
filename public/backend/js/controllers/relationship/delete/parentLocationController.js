c42e9428df7ec6fb50d57fad87f7b16097912e69
Nicho <nicho90@web.de>
2017-01-31 15:48:13 +0100

    Implemented deleteControllers for relationships in backend

---
+++ public/backend/js/controllers/relationship/delete/parentLocationController.js
@@ -0,0 +1,60 @@
+var app = angular.module("ive");
+
+// Relationship parent_location delete controller
+app.controller("parentLocationDeleteController", function($scope, $rootScope, $routeParams, $translate, $location, config, $window, $authenticationService, $relationshipService) {
+
+    /*************************************************
+        FUNCTIONS
+     *************************************************/
+
+    /**
+     * [changeTab description]
+     * @param  {[type]} tab [description]
+     * @return {[type]}     [description]
+     */
+    $scope.changeTab = function(tab){
+        $scope.tab = tab;
+    };
+
+    /**
+     * [redirect description]
+     * @param  {[type]} path [description]
+     * @return {[type]}      [description]
+     */
+    $scope.redirect = function(path){
+        $location.url(path);
+    };
+
+    /**
+     * [delete description]
+     * @return {[type]} [description]
+     */
+    $scope.delete = function(){
+        $scope.changeTab(0);
+        $relationshipService.remove($scope.relationship.relationship_id)
+        .then(function onSuccess(response) {
+            $scope.redirect("/relationship/parent_location");
+        })
+        .catch(function onError(response) {
+            $window.alert(response.data);
+        });
+    };
+
+
+    /*************************************************
+        INIT
+     *************************************************/
+    $scope.changeTab(0);
+    $scope.input = "";
+    $scope.relationship_type = "PARENT_LOCATION";
+
+    $relationshipService.retrieve_by_id('parent_location', $routeParams.relationship_id)
+    .then(function onSuccess(response) {
+        $scope.relationship = response.data;
+        $scope.changeTab(1);
+    })
+    .catch(function onError(response) {
+        $window.alert(response.data);
+    });
+
+});
