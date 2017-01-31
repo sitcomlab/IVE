43f550650f71a6d27d5e721ca0d428523ec2563a
Nicho <nicho90@web.de>
2017-01-31 02:24:45 +0100

    Implemented apiController for relationships
    to get a relationship by id

--- routes/relationships.js
+++ routes/relationships.js
@@ -1,28 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-var get = require('../controllers/relationships/get');
-var belongs_to = require('../controllers/relationships/belongs_to');
-var connected_to = require('../controllers/relationships/connected_to');
-var embedded_in = require('../controllers/relationships/embedded_in');
-var parent_location = require('../controllers/relationships/parent_location');
-var recorded_at = require('../controllers/relationships/recorded_at');
+//var get = require('../controllers/relationships/get');
+var list_belongs_to = require('../controllers/relationships/list/belongs_to');
+var list_connected_to = require('../controllers/relationships/list/connected_to');
+var list_embedded_in = require('../controllers/relationships/list/embedded_in');
+var list_parent_location = require('../controllers/relationships/list/parent_location');
+var list_recorded_at = require('../controllers/relationships/list/recorded_at');
+
+//var get_belongs_to = require('../controllers/relationships/get/belongs_to');
+var get_connected_to = require('../controllers/relationships/get/connected_to');
+var get_embedded_in = require('../controllers/relationships/get/embedded_in');
+var get_parent_location = require('../controllers/relationships/get/parent_location');
+var get_recorded_at = require('../controllers/relationships/get/recorded_at');
 
 
 
 // LIST BY RELATIONSHIP-TYPE
-router.get('/relationship/belongs_to/:label', belongs_to.request);
+router.get('/relationship/belongs_to/:label', list_belongs_to.request);
 
-router.get('/relationship/connected_to', connected_to.request);
+router.get('/relationship/connected_to', list_connected_to.request);
 
-router.get('/relationship/embedded_in', embedded_in.request);
+router.get('/relationship/embedded_in', list_embedded_in.request);
 
-router.get('/relationship/parent_location', parent_location.request);
+router.get('/relationship/parent_location', list_parent_location.request);
 
-router.get('/relationship/recorded_at', recorded_at.request);
+router.get('/relationship/recorded_at', list_recorded_at.request);
+
+// GET
+//router.get('/relationships/:relationship_id', get.request);
 
 // GET BY ID
-router.get('/relationships/:relationship_id', get.request);
+//router.get('/relationship/belongs_to/:relationship_id/:label', get_belongs_to.request);
+
+router.get('/relationship/connected_to/:relationship_id', get_connected_to.request);
+
+router.get('/relationship/embedded_in/:relationship_id', get_embedded_in.request);
+
+router.get('/relationship/parent_location/:relationship_id', get_parent_location.request);
+
+router.get('/relationship/recorded_at/:relationship_id', get_recorded_at.request);
 
 
 module.exports = router;
