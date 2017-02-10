d7490e0388025fdfdfe0ca783aa33ee0bfb2dddb
Nicho <nicho90@web.de>
2017-02-10 17:16:18 +0100

    Extended server with Postgres-connection
    and extended setup with import function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var neo4j = require('neo4j-driver').v1;
-
+var pg = require('pg');
 
 // Create server
 var app = express();
@@ -17,6 +17,12 @@ var db_host = process.env.DB_HOST || '127.0.0.1';
 var db_port = process.env.DB_PORT || '7687';
 var db_user = process.env.DB_USER || 'neo4j';
 var db_password = process.env.DB_PASSWORD || '123456';
+var postgres_host = process.env.POSTGRES_HOST || 'localhost';
+var postgres_port = process.env.POSTGRES_PORT || 5432;
+var postgres_db_name = process.env.POSTGRES_DB_NAME || 'ive';
+var postgres_user = process.env.POSTGRES_USER || 'Nicho';
+var postgres_password = process.env.POSTGRES_PASSWORD || undefined;
+var postgres_ssl = process.env.POSTGRES_DB_SSL || false;
 var backend_user = process.env.BACKEND_USER || 'admin';
 var backend_password = process.env.BACKEND_PASSWORD || 'admin';
 var jwtSecret = process.env.JWTSECRET || 'superSecretKey';
@@ -26,6 +32,15 @@ exports.backend_account = {
     username: backend_user,
     password: backend_password
 };
+var pool = new pg.Pool({
+    user: postgres_user,
+    password: postgres_password,
+    host: postgres_host,
+    port: postgres_port,
+    database: postgres_db_name,
+    ssl: JSON.parse(postgres_ssl)
+});
+exports.pool = pool;
 
 
 // Connect to Neo4j
@@ -37,11 +52,33 @@ session
     .run(query)
     .then(function(result) {
         session.close();
-        console.log(colors.blue(new Date() + " Neo4j is running on Port 7474"));
+        console.log(colors.blue(new Date() + " Neo4j is running on Port " + db_port));
     })
     .catch(function(err) {
         console.error(colors.red(new Date() + " Neo4j could not been accessed:\n" + JSON.stringify(err)));
     });
+
+
+// Connect to Postgres
+pool.connect(function(err, client, done) {
+    if(err) {
+        console.error(err);
+        console.error(colors.red(new Date() + " Postgres is not running!"));
+    } else {
+        client.query("SELECT true;", function(err, result) {
+            done();
+            if (err) {
+                console.error(colors.red(JSON.stringify(err)));
+            } else {
+                console.log(colors.blue(new Date() + " Postgres is running on Port " + postgres_port));
+            }
+        });
+    }
+});
+
+pool.on('error', function (err, client) {
+    console.error('idle client error', err.message, err.stack);
+});
 
 
 // Load certificstes
--- setup.js
+++ setup.js
@@ -7,10 +7,52 @@ var db_host = process.env.DB_HOST || '127.0.0.1';
 var db_port = process.env.DB_PORT || '7687';
 var db_user = process.env.DB_USER || 'neo4j';
 var db_password = process.env.DB_PASSWORD || '123456';
+var postgres_host = process.env.POSTGRES_HOST || 'localhost';
+var postgres_port = process.env.POSTGRES_PORT || 5432;
+var postgres_db_name = process.env.POSTGRES_DB_NAME || 'ive';
+var postgres_user = process.env.POSTGRES_USER || 'Nicho';
+var postgres_password = process.env.POSTGRES_PASSWORD || undefined;
+var postgres_ssl = process.env.POSTGRES_DB_SSL || false;
+exports.server_url = server_url;
+exports.jwtSecret = jwtSecret;
+exports.backend_account = {
+    username: backend_user,
+    password: backend_password
+};
+var pool = new pg.Pool({
+    user: postgres_user,
+    password: postgres_password,
+    host: postgres_host,
+    port: postgres_port,
+    database: postgres_db_name,
+    ssl: JSON.parse(postgres_ssl)
+});
+exports.pool = pool;
 
 // Connect to Neo4j
 var driver = neo4j.driver("bolt://" + db_host + ":" + db_port, neo4j.auth.basic(db_user, db_password));
 var session = driver.session();
+
+// Connect to Postgres
+pool.connect(function(err, client, done) {
+    if(err) {
+        console.error(err);
+        console.error(colors.red(new Date() + " Postgres is not running!"));
+    } else {
+        client.query("SELECT true;", function(err, result) {
+            done();
+            if (err) {
+                console.error(colors.red(JSON.stringify(err)));
+            } else {
+                console.log(colors.blue(new Date() + " Postgres is running on Port " + postgres_port));
+            }
+        });
+    }
+});
+
+pool.on('error', function (err, client) {
+    console.error('idle client error', err.message, err.stack);
+});
 
 // Load files
 var dir = "/queries/setup/";
@@ -38,23 +80,49 @@ queries.push(fs.readFileSync(__dirname + dir + 'import_connected_to.cypher', 'ut
 queries.push(fs.readFileSync(__dirname + dir + 'import_recorded_at.cypher', 'utf8').toString());
 queries.push(fs.readFileSync(__dirname + dir + 'import_embedded_in.cypher', 'utf8').toString());
 
+async.waterfall([
+    function(callback){ // Import graph in neo4j
+        async.forEachOf(queries, function (query, key, callback) {
+            // Run query
+            session
+                .run(query)
+                .then(function(result) {
+                    console.log(colors.blue(query));
+                    console.log(colors.green("Done!"));
+                    callback(null);
+                })
+                .catch(function(err) {
+                    callback(err);
+                });
+        }, function(err){
+            // Close connection
+            session.close();
+            callback();
+            if(err) {
+                callback(err);
 
-async.forEachOf(queries, function (query, key, callback) {
-    // Run query
-    session
-        .run(query)
-        .then(function(result) {
-            console.log(colors.blue(query));
-            console.log(colors.green("Done!"));
-            callback(null);
-        })
-        .catch(function(err) {
-            callback(err);
+            } else {
+                callback(null);
+            }
         });
-}, function(err){
-    // Close connection
-    session.close();
+    },
+    function(callback){ // Import intents in Postgres
+        var query = "COPY Intents FROM '" + __dirname + inputFile + "' WITH DELIMITER ','" + headers +  ";";
 
+        // Database query
+        client.query(query, function(err, result) {
+            done();
+            if (err) {
+                callback(err);
+            } else {
+                console.log(colors.blue(query));
+                console.log(colors.green("Done!"));
+
+                callback(null);
+            }
+        });
+    }
+], function(err){
     if(err) {
         console.error(colors.red(JSON.stringify(err)));
     } else {
