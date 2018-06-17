0071ea46f4dbe87fe1f802df9f58c6308a05bfed
Albert <Alf@Alberts-MacBook-Pro.local>
2018-06-17 20:49:31 +0200

    hide & flip functions
    hide textarea or rating area after push the submitbtn

--- public/feedback/js/controllers/mainController.js
+++ public/feedback/js/controllers/mainController.js
@@ -21,6 +21,39 @@ app.controller("mainController", function($scope, $socket) {
             rating: $scope.feedback.rating,
             comment: $scope.feedback.comment
         });
+        if ($scope.feedback.comment !== "" && $scope.feedback.rating === null){
+            $scope.showMeFlipTextarea = true;
+
+        }else { if ( $scope.feedback.rating === "Like" || $scope.feedback.rating === "Dislike" ) {
+
+                    $scope.showMeFlipRating = true;
+                } else {console.log("if error")}
+        }
+
+        if( $scope.showMeFlipTextarea === true && $scope.showMeFlipRating === true){
+            $("#feedbackSubmitBtn").slideUp("slow");
+        }
+
     };
+
+        // show text area and hide ratingArea
+        $("#flipTextarea").click(function() {
+            $("#comment").slideDown("slow");
+            $("#ratingArea").slideUp("slow");
+            $("#feedbackSubmitBtn").slideDown("slow");
+            //reset rating
+            $scope.feedback = {rating : null};
+
+        });
+
+        // show rating area and hide text area
+        $("#flipRating").click(function() {
+            $("#ratingArea").slideDown("slow");
+            $("#comment").slideUp("slow");
+            $("#feedbackSubmitBtn").slideDown("slow");
+            //reset comment
+            $scope.feedback = {comment : ""};
+
+        });
 
 });
