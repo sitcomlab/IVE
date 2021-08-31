3be4f93bf54f19d3b8a6b1784776f26225c0612c
Norwin <git@nroo.de>
2021-08-31 23:38:29 +0200

    creator: avoid login & redirect on reload
    ..to improve usability, especially during development:
    - no more redirects if not logged in (modal instead of route+redirects)
    - no more login on each reload (powered by `localStorage`)

--- public/creator/js/services/authenticationService.js
+++ public/creator/js/services/authenticationService.js
@@ -1,10 +1,11 @@
-var app = angular.module("authenticationService", []);
+const user_storage_key = 'AUTH_USER';
 
+var app = angular.module("authenticationService", []);
 
 // User service
 app.factory('$authenticationService', function($http, $log, config) {
 
-    var authenticated_user;
+    let authenticated_user = JSON.parse(localStorage.getItem(user_storage_key));
 
     return {
         init: function(){
@@ -20,30 +21,19 @@ app.factory('$authenticationService', function($http, $log, config) {
             authenticated_user = data;
         },
         authenticated: function(){
-            if(authenticated_user !== undefined){
-                return true;
-            } else {
-                return false;
-            }
-        },
-        setToken: function(data){
-            authenticated_user.token = data;
-        },
-        getToken: function(){
-            if(authenticated_user !== undefined){
-                if(authenticated_user.token !== undefined){
-                    return authenticated_user.token;
-                } else {
-                    return undefined;
-                }
-            } else {
-                return undefined;
-            }
+            return !!authenticated_user;
         },
         login: function(data) {
-            return $http.post(config.getApiEndpoint() + "/login", data);
+            return $http.post(config.getApiEndpoint() + "/login", data)
+                .then(res => { 
+                    authenticated_user = res.data;
+                    localStorage.setItem(user_storage_key, JSON.stringify(authenticated_user));
+                    return res.data;
+                })
+        },
+        logout: function(){
+            authenticated_user = null;
+            localStorage.removeItem(user_storage_key);
         }
-
     };
-
 });
